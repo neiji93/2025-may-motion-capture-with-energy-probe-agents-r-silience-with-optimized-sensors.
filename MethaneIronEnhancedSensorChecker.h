c1d3c1d6fbef47362a22df28c6609fc0ce7e9e85
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-03-30 17:07:06 +0200

    Update MethaneIronEnhancedSensorChecker.h

--- MethaneIronEnhancedSensorChecker.h
+++ MethaneIronEnhancedSensorChecker.h
@@ -1,1 +1,9 @@
 //streaming can be easiest and powerfull.
+
+//the signals can have no immediate logics.
+class dualMetalSignalToCPUsServers : public baseSignalFunctionPointer
+{
+public:
+   void
+}
+
